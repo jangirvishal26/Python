7b8e5974335109f4892f1c48fba213525dc98a76
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-11-21 12:28:59 +0530

    Update path_traversal.py

--- path_traversal.py
+++ path_traversal.py
@@ -1,30 +1,29 @@
 import os.path
-from flask import Flask, request, abort
+from flask import Flask, request, abort, send_file
 
 app = Flask(__name__)
+
+def is_valid_file_path(filename):
+    # Implement your logic to validate the filename
+    # For example, check if the filename is in an allowed directory
+    allowed_directory = "/path/to/allowed_directory"
+    full_path = os.path.join(allowed_directory, filename)
+    return os.path.exists(full_path) and full_path.startswith(allowed_directory)
+
+def get_file_data(filename):
+    # Implement your logic to read and return the file data
+    # Here, we use Flask's send_file function for better security
+    return send_file(filename, as_attachment=True)
 
 @app.route("/user_picture1")
 def user_picture1():
     filename = request.args.get('p')
-    # BAD: This could read any file on the file system
-    data = open(filename, 'rb').read()
-    return data
 
-@app.route("/user_picture2")
-def user_picture2():
-    base_path = '/server/static/images'
-    filename = request.args.get('p')
-    # BAD: This could still read any file on the file system
-    data = open(os.path.join(base_path, filename), 'rb').read()
-    return data
+    if is_valid_file_path(filename):
+        return get_file_data(filename)
+    else:
+        # Handle invalid file requests, for example, return an error response
+        return "Invalid file request", 400
 
-@app.route("/user_picture3")
-def user_picture3():
-    base_path = '/server/static/images'
-    filename = request.args.get('p')
-    #GOOD -- Verify with normalised version of path
-    fullpath = os.path.normpath(os.path.join(base_path, filename))
-    if not fullpath.startswith(base_path):
-        raise Exception("not allowed")
-    data = open(fullpath, 'rb').read()
-    return data
+if __name__ == '__main__':
+    app.run(debug=True)
