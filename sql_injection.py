294fcac456073a72dc914ea22d15d144344e6dc1
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-11-22 17:08:28 +0530

    Update sql_injection.py

--- sql_injection.py
+++ sql_injection.py
@@ -1,19 +1,10 @@
-from django.conf.urls import url
 from django.db import connection
 
-
-def show_user(request, username):
+def insecure_login(username, password):
     with connection.cursor() as cursor:
-        # BAD -- Using string formatting
-        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
+        # VULNERABLE -- Using string formatting
+        query = "SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (username, password)
+        cursor.execute(query)
         user = cursor.fetchone()
 
-        # GOOD -- Using parameters
-        cursor.execute("SELECT * FROM users WHERE username = %s", username)
-        user = cursor.fetchone()
-
-        # BAD -- Manually quoting placeholder (%s)
-        cursor.execute("SELECT * FROM users WHERE username = '%s'", username)
-        user = cursor.fetchone()
-
-urlpatterns = [url(r'^users/(?P<username>[^/]+)$', show_user)]
+    return user
