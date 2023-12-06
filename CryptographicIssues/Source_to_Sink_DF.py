1c468260c290e6b7bd37705f6f39e467e994d40b
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-06 14:28:37 +0530

    Update Source_to_Sink_DF.py

--- CryptographicIssues/Source_to_Sink_DF.py
+++ CryptographicIssues/Source_to_Sink_DF.py
@@ -1,19 +1,1 @@
-from Crypto.Cipher import DES, AES
 
-def encrypt_message(cipher, message):
-    return cipher.encrypt(message)
-
-def send_message(channel, encrypted_message):
-    channel.send(encrypted_message)
-
-# Example usage with DES
-des_cipher = DES.new(SECRET_KEY)
-message_to_encrypt = "Your message"
-encrypted_message = encrypt_message(des_cipher, message_to_encrypt)
-send_message(channel, encrypted_message)
-
-# Example usage with AES
-aes_cipher = AES.new(SECRET_KEY)
-message_to_encrypt = "Your message"
-encrypted_message = encrypt_message(aes_cipher, message_to_encrypt)
-send_message(channel, encrypted_message)
