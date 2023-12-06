a458ff876a90a4cef2ac331cad3c3a50ad229891
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-06 16:44:01 +0530

    Update demo.py

--- AuthenticationOverHTTP/demo.py
+++ AuthenticationOverHTTP/demo.py
@@ -1,30 +1,14 @@
-from flask import Flask, request, session, redirect
-
-app = Flask(__name__)
-
-# initiate the session in order to validate sessions
-app.secret_key = 'your_secret_key'
-
-# If the session is registered to a valid user, then allow update
-@app.route('/update_profile', methods=['POST'])
-def update_profile():
-    # Check if the session is registered to a valid user
-    if 'username' not in session:
-        return "Invalid session detected! Redirect user to login page..."
-
-    # The user session is valid, so process the request and update the information
-    username = session['username']
-    email = request.form.get('email')
-    
-    # Send an update to the database (replace this with your actual database update logic)
-    send_update_to_database(username, email)
-
-    return "Your profile has been successfully updated."
-
-# Function to simulate sending an update to the database
-def send_update_to_database(username, email):
-    # Replace this with your actual database update logic
-    print(f"Updating database for user {username} with email {email}")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@authentication_decorator
+@csrf_exempt
+def csrf_transfer_monei(request):
+    if request.method == 'GET':
+        try:
+            cookie = request.COOKIES['auth_cookiee']
+            payload = jwt.decode(cookie, 'csrf_vulneribility', algorithms=['HS256'])
+            username = payload['username']
+            User = CSRF_user_tbl.objects.filter(username=username)
+            if not User:
+                redirect('/mitre/9/lab/login')
+            return render(request, 'mitre/csrf_dashboard.html', {'balance': User[0].balance})
+        except:
+            return redirect('/mitre/9/lab/login')
