1a350cc7f0fca308e0526e753436627832094136
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-06 11:13:50 +0530

    Update Command-Injection.py

--- Injection/Command-Injection.py
+++ Injection/Command-Injection.py
@@ -1,5 +1,18 @@
-def command_out(command):
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return process.communicate()
+def get_expression():
+    compute = input('\nYour expression? => ')
+    if not compute:
+        print("No input")
+        return None
+    return compute
 
-command_out("rm  -rf *")
+def process_expression(expression):
+    try:
+        result = eval(expression)
+        print("Result =", result)
+    except Exception as e:
+        print("Error:", e)
+
+if __name__ == "__main__":
+    expression = get_expression()
+    if expression:
+        process_expression(expression)
