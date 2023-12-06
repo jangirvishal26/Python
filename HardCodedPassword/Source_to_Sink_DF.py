3b18d39c320d28e786aed06400b5022451ab30a6
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-06 12:07:07 +0530

    Create Source_to_Sink_DF.py

---
+++ HardCodedPassword/Source_to_Sink_DF.py
@@ -0,0 +1,29 @@
+import requests
+
+# BAD: Hardcoded credentials
+USERNAME = "admin"
+PASSWORD = "secretpassword"
+
+def authenticate(username, password):
+    if username == USERNAME and password == PASSWORD:
+        return True
+    else:
+        return False
+
+def login(request):
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    if authenticate(username, password):
+        return "Login successful"
+    else:
+        return "Login failed"
+
+# Example Flask route using the login function
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/login')
+def login_route():
+    return login(request)
