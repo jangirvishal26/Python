44ce195c241ff2e877f9ee16a7b9f072b5d7ea6a
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-06 16:46:53 +0530

    Create Source_to_Sink_SF.py

---
+++ CSRF/Source_to_Sink.py
@@ -0,0 +1,14 @@
+@authentication_decorator
+@csrf_exempt
+def csrf_transfer_monei(request):
+    if request.method == 'GET':
+        try:
+            cookie = request.COOKIES['auth_cookiee']
+            payload = jwt.decode(cookie, 'csrf_vulneribility', algorithms=['HS256'])
+            username = payload['username']
+            User = CSRF_user_tbl.objects.filter(username=username)
+            if not User:
+                redirect('/mitre/9/lab/login')
+            return render(request, 'mitre/csrf_dashboard.html', {'balance': User[0].balance})
+        except:
+            return redirect('/mitre/9/lab/login')
