b4a2c9cc0427d3e97920346bfc237c6a3607de4f
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-06 11:26:57 +0530

    Update Source_to_Sink_SF.py

--- Injection/Command-Injection/Source_to_Sink_SF.py
+++ Injection/Command-Injection/Source_to_Sink_SF.py
@@ -1,1 +1,13 @@
-print("hello")
+def process_expression():
+    compute = input('\nYour expression? => ')
+    if not compute:
+        print("No input")
+    else:
+        try:
+            result = eval(compute)
+            print("Result =", result)
+        except Exception as e:
+            print("Error:", e)
+
+if __name__ == "__main__":
+    process_expression()
