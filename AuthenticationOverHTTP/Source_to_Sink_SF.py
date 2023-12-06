a5716ffe6e0628c1734626d44005fe127c340ab9
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-06 14:50:22 +0530

    Update Source_to_Sink_SF.py

--- AuthenticationOverHTTP/Source_to_Sink_SF.py
+++ AuthenticationOverHTTP/Source_to_Sink_SF.py
@@ -1,17 +1,38 @@
-from flask import Flask, request, jsonify
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
 
-app = Flask(__name__)
+class SimpleRequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length).decode('utf-8')
 
-# Insecure authentication over HTTP
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+        # Parse the form data
+        form_data = parse_qs(body)
 
-    if username == 'admin' and password == 'password':
-        return jsonify({'message': 'Login successful'})
-    else:
-        return jsonify({'message': 'Login failed'})
+        # Retrieve username and password from form data
+        username = form_data.get('username', [''])[0]
+        password = form_data.get('password', [''])[0]
+
+        # Authenticate the user
+        response_message = self.authenticate(username, password)
+
+        # Send the response
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(response_message.encode('utf-8'))
+
+    def authenticate(self, username, password):
+        if username == 'admin' and password == 'password':
+            return '{"message": "Login successful"}'
+        else:
+            return '{"message": "Login failed"}'
+
+def run_server(port=5000):
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, SimpleRequestHandler)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    run_server()
