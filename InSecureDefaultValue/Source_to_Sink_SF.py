06982eb9ff9771bf86d2aa8e198e2f02bb81f5d4
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-07 12:15:17 +0530

    Update Source_to_Sink_SF.py

--- InSecureDefaultValue/Source_to_Sink_SF.py
+++ InSecureDefaultValue/Source_to_Sink_SF.py
@@ -1,8 +1,16 @@
-# Insecure default variable initialization example (hypothetical)
-class User:
-    def __init__(self, username='admin', password='weak_password'):
-        self.username = username
-        self.password = password
+# $user and $pass automatically set from POST request
+user = request.form.get('user')  # Assuming Flask-like request object
+passw = request.form.get('pass')  # Assuming Flask-like request object
 
-# Usage
-user = User()
+def login_user(user, passw):
+    # Assuming login_user is a function that checks the credentials
+    # and returns True if the user is successfully logged in
+    return login_user(user, passw)
+
+authorized = False
+
+if login_user(user, passw):
+    authorized = True
+
+if authorized:
+    generate_page()
