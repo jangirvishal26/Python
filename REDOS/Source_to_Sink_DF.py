538f84f30383a0c2605ec69931ede1d33ef52420
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-07 12:01:29 +0530

    Create Source_to_Sink_DF.py

---
+++ REDOS/Source_to_Sink_DF.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import re
+
+app = Flask(__name__)
+
+def get_user_input():
+    return request.args['regex'], request.args['data']
+
+def perform_regex_search(regex, data):
+    re.search(regex, data)  # Noncompliant
+
+@app.route('/lookup')
+def lookup():
+    regex, data = get_user_input()
+    perform_regex_search(regex, data)
+    # Rest of the code
