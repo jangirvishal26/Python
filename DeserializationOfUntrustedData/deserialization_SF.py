d0897db53509817e285650feb18cd2ae6ba5ca0b
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-12 14:29:55 +0530

    Update deserialization_SF.py

--- DeserializationOfUntrustedData/deserialization_SF.py
+++ DeserializationOfUntrustedData/deserialization_SF.py
@@ -2,7 +2,14 @@ from django.conf.urls import url
 import pickle
 
 def unsafe(pickled):
-    return pickle.loads(pickled)
+    data = pickle.loads(pickled)
+    # Call a method to process the deserialized data (in this case, print the data)
+    print_data(data)
+    return "Data printed successfully."
+
+def print_data(data):
+    # Implement your logic to print or process the deserialized data
+    print("Deserialized data:", data)
 
 urlpatterns = [
     url(r'^(?P<object>.*)$', unsafe)
