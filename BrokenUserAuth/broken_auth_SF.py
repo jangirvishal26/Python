32b4bed4204b10bb68476e48563816c8a1f3da9d
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-12 13:08:30 +0530

    Update broken_auth_SF.py

--- BrokenUserAuth/broken_auth_SF.py
+++ BrokenUserAuth/broken_auth_SF.py
@@ -1,32 +1,16 @@
-from flask import Flask, request, redirect
+from http import cookies
 
-app = Flask(__name__)
+q = cookies.SimpleCookie()
 
-# Dummy user data (for demonstration purposes)
-users = {'admin': 'admin_password', 'user1': 'password1', 'user2': 'password2'}
-
-# Route for handling login
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-    # Simulate authentication (plain text passwords for demonstration purposes)
-    if username in users and users[username] == password:
-        return f'Welcome, {username}!'
+if q.get('loggedin', '') != 'true':
+    # Assuming AuthenticateUser and ExitError functions are defined elsewhere
+    if not AuthenticateUser(q.get('username', ''), q.get('password', '')):
+        ExitError("Error: you need to log in first")
     else:
-        return 'Invalid username or password.'
+        # Set loggedin and user cookies.
+        q['loggedin'] = 'true'
+        q['user'] = q.get('username', '')
 
-# Route for an admin-only resource
-@app.route('/admin/dashboard')
-def admin_dashboard():
-    username = request.args.get('username')
-
-    # Simulate an authorization check (insecure implementation)
-    if username == 'admin':
-        return 'Admin dashboard content.'
-    else:
-        return 'Unauthorized access.'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if q.get('user', '') == 'Administrator':
+    # Assuming DoAdministratorTasks function is defined elsewhere
+    DoAdministratorTasks()
