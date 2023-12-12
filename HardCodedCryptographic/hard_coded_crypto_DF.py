69a181825890e2be51048d4ba35b93d8d244a6d7
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-12 14:59:32 +0530

    Update hard_coded_crypto_DF.py

--- HardCodedCryptographic/hard_coded_crypto_DF.py
+++ HardCodedCryptographic/hard_coded_crypto_DF.py
@@ -1,38 +1,26 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-import base64
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 
-# WARNING: This is a hardcoded key and should NEVER be used in production
-HARDCODED_KEY = b'Sixteen byte key'
+# Hardcoded cryptographic key (vulnerable)
+encryption_key = b'SuperSecretKey123'
 
-def encrypt_data(data):
-    cipher = AES.new(HARDCODED_KEY, AES.MODE_EAX)
-    nonce = cipher.nonce
-    ciphertext, tag = cipher.encrypt_and_digest(data.encode('utf-8'))
-    return base64.b64encode(nonce + tag + ciphertext).decode('utf-8')
+def encrypt(data):
+    cipher = Cipher(algorithms.AES(encryption_key), modes.ECB(), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(data) + encryptor.finalize()
+    return ciphertext
 
-def decrypt_data(encrypted_data):
-    encrypted_data = base64.b64decode(encrypted_data)
-    nonce = encrypted_data[:16]
-    tag = encrypted_data[16:32]
-    ciphertext = encrypted_data[32:]
-    cipher = AES.new(HARDCODED_KEY, AES.MODE_EAX, nonce=nonce)
-    return cipher.decrypt_and_verify(ciphertext, tag).decode('utf-8')
+def decrypt(ciphertext):
+    cipher = Cipher(algorithms.AES(encryption_key), modes.ECB(), backend=default_backend())
+    decryptor = cipher.decryptor()
+    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+    return plaintext
 
-def get_sensitive_information():
-    # Source: Retrieve sensitive information (plaintext data)
-    return 'Sensitive information'
+# Example usage
+data_to_encrypt = b'This is sensitive data.'
+encrypted_data = encrypt(data_to_encrypt)
+decrypted_data = decrypt(encrypted_data)
 
-def process_sensitive_information(data):
-    # Sink: Process sensitive information (encrypt data)
-    encrypted_data = encrypt_data(data)
-    print(f'Encrypted data: {encrypted_data}')
-
-    # Sink: Process sensitive information (decrypt data)
-    decrypted_data = decrypt_data(encrypted_data)
-    print(f'Decrypted data: {decrypted_data}')
-
-if __name__ == '__main__':
-    # Example usage
-    sensitive_data = get_sensitive_information()
-    process_sensitive_information(sensitive_data)
+print(f"Original Data: {data_to_encrypt.decode()}")
+print(f"Encrypted Data: {encrypted_data}")
+print(f"Decrypted Data: {decrypted_data.decode()}")
