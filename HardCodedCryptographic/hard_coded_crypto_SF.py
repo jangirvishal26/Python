7f2fc7cd97299de69c8e41bcfaeda8ec51ef247a
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-12 14:51:48 +0530

    Update hard_coded_crypto_SF.py

--- HardCodedCryptographic/hard_coded_crypto_SF.py
+++ HardCodedCryptographic/hard_coded_crypto_SF.py
@@ -1,31 +1,12 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-import base64
+def verify_admin(password):
+    if password != "68af404b513073584c4b6f22b6c63e6b":
+        print("Incorrect Password!")
+        return 0
 
-# WARNING: This is a hardcoded key and should NEVER be used in production
-HARDCODED_KEY = b'Sixteen byte key'
+    print("Entering Diagnostic Mode...")
+    return 1
 
-def encrypt_data(data):
-    cipher = AES.new(HARDCODED_KEY, AES.MODE_EAX)
-    nonce = cipher.nonce
-    ciphertext, tag = cipher.encrypt_and_digest(data.encode('utf-8'))
-    return base64.b64encode(nonce + tag + ciphertext).decode('utf-8')
-
-def decrypt_data(encrypted_data):
-    encrypted_data = base64.b64decode(encrypted_data)
-    nonce = encrypted_data[:16]
-    tag = encrypted_data[16:32]
-    ciphertext = encrypted_data[32:]
-    cipher = AES.new(HARDCODED_KEY, AES.MODE_EAX, nonce=nonce)
-    decrypted_data = cipher.decrypt_and_verify(ciphertext, tag)
-    return decrypted_data.decode('utf-8')
-
-if __name__ == '__main__':
-    # Example usage
-    original_data = 'Sensitive information'
-    encrypted_data = encrypt_data(original_data)
-    decrypted_data = decrypt_data(encrypted_data)
-
-    print(f'Original data: {original_data}')
-    print(f'Encrypted data: {encrypted_data}')
-    print(f'Decrypted data: {decrypted_data}')
+# Example usage:
+password_input = input("Enter the password: ")
+result = verify_admin(password_input)
+print("Verification result:", result)
