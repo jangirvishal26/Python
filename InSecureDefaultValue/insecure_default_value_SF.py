67deaca17b506a380a097ba9111f4f5672ce54ab
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-14 14:13:15 +0530

    Update insecure_default_value_SF.py

--- InSecureDefaultValue/insecure_default_value_SF.py
+++ InSecureDefaultValue/insecure_default_value_SF.py
@@ -1,15 +1,6 @@
 # $user and $pass automatically set from POST request
-user = request.form.get('user')  # Assuming Flask-like request object
-passw = request.form.get('pass')  # Assuming Flask-like request object
 
-def login_user(user, passw):
-    # Assuming login_user is a function that checks the credentials
-    # and returns True if the user is successfully logged in
-    return login_user(user, passw)
-
-authorized = False
-
-if login_user(user, passw):
+if login_user(user, pass):
     authorized = True
 
 if authorized:
