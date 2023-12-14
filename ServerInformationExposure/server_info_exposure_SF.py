d104a4c0d21662fb68c96b5be6e81d45939571db
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-14 15:41:00 +0530

    Update server_info_exposure_SF.py

--- ServerInformationExposure/server_info_exposure_SF.py
+++ ServerInformationExposure/server_info_exposure_SF.py
@@ -1,28 +1,6 @@
-from flask import Flask, request, abort
-
-app = Flask(__name__)
-
-@app.route('/divide')
-def divide():
-    try:
-        numerator = int(request.args.get('numerator'))
-        denominator = int(request.args.get('denominator'))
-
-        # Processing the user input (source and sink combined)
-        try:
-            result = numerator / denominator
-            return f"The result of the division is: {result}"
-        except ZeroDivisionError as e:
-            # Log the error for internal use
-            app.logger.error(f"Error: Division by zero. {str(e)}")
-            # Return a generic message to the client
-            abort(400, "Invalid input. Cannot divide by zero.")
-        except Exception as e:
-            # Log the error for internal use
-            app.logger.error(f"An error occurred: {str(e)}")
-            # Return a generic message to the client
-            abort(500, "An unexpected error occurred.")
-
-    except ValueError as e:
-        # Log the error for internal use
-        app.logger.error(f"Error: Invalid input. {str(e
+try:
+    # Simulating a database connection error
+    raise Exception("Failed to connect to the database. Check your credentials at: /path/to/config.txt")
+except Exception as e:
+    # CWE-209: Printing the exception message containing sensitive information
+    print(f"Error: {e}")
