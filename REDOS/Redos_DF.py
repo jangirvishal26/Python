f5e83abf61b53c5577f604da57bbe155311e2585
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-14 15:32:23 +0530

    Update Redos_DF.py

--- REDOS/Redos_DF.py
+++ REDOS/Redos_DF.py
@@ -7,10 +7,10 @@ def get_user_input():
     return request.args['regex'], request.args['data']
 
 def perform_regex_search(regex, data):
-    re.search(regex, data)  # Noncompliant
+    re.search(regex, data)
 
 @app.route('/lookup')
 def lookup():
     regex, data = get_user_input()
     perform_regex_search(regex, data)
-    # Rest of the code
+    
