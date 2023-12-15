70da9e64bb28a687d46eaf99307e6c17fa3529fb
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-15 12:09:54 +0530

    Update server_info_exposure_SF.py

--- ServerInformationExposure/server_info_exposure_SF.py
+++ ServerInformationExposure/server_info_exposure_SF.py
@@ -1,6 +1,10 @@
 try:
-    # Simulating a database connection error
-    raise Exception("Failed to connect to the database. Check your credentials at: /path/to/config.txt")
+    # Simulating a database connection error with sensitive information
+    raise Exception(f"Failed to connect to the database. Check your credentials at: {get_sensitive_file_path()}")
 except Exception as e:
     # CWE-209: Printing the exception message containing sensitive information
     print(f"Error: {e}")
+
+def get_sensitive_file_path():
+    # Simulating a function that returns a sensitive file path
+    return "/path/to/config.txt"
