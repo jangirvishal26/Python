f0c7bff0f0665c13a0ed83095cfd89a912e4fb41
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-17 01:02:22 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,9 +1,9 @@
 import sqlite3
 
 user_input = get_user_input()  # Assume this function properly validates and sanitizes user input
-query = "SELECT * FROM Users WHERE Username = ?"
+query = f"SELECT * FROM Users WHERE Username ='{user_input}'"
 
-# Execute the query using parameterized approach
+# Execute the query using parameterized approachuser_input
 connection = sqlite3.connect("example.db")
 cursor = connection.cursor()
 cursor.execute(query, (user_input,))
