66e38c6e40b4694c5b87bcb52147330216b118a7
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2023-12-18 14:29:56 +0530

    Update css_SF.py

--- CrossSiteScripting/css_SF.py
+++ CrossSiteScripting/css_SF.py
@@ -1,17 +1,26 @@
-from flask import Flask, request, make_response, escape
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/combined')
-def combined():
-    # Source and sink are combined in the same function
+@app.route('/xss')
+def xss_vulnerable():
+    # Source: User input from the 'name' parameter
     user_input = request.args.get('name', '')
     
-    # Processing the input (sink)
-    response_content = "Your name is " + escape(user_input)
+    # Sink: Incorporating user input into an HTML template without proper escaping
+    html_template = """
+    <html>
+    <head><title>XSS Vulnerable Page</title></head>
+    <body>
+        <h1>Hello, {{ user_input }}!</h1>
+    </body>
+    </html>
+    """
+    
+    # Rendering the template without proper escaping
+    rendered_template = render_template_string(html_template, user_input=user_input)
 
-    # Returning the response
-    return make_response(response_content)
+    return rendered_template
 
 if __name__ == '__main__':
     app.run(debug=True)
