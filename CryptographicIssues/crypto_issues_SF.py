d9a78c54651b9e5efef4170361f7987f16c02a53
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2024-01-04 11:37:26 +0530

    Update crypto_issues_SF.py
    CWE -  310

--- CryptographicIssues/crypto_issues_SF.py
+++ CryptographicIssues/crypto_issues_SF.py
@@ -3,10 +3,10 @@ from Crypto.Cipher import DES, AES
 cipher = DES.new(SECRET_KEY)
 
 def send_encrypted(channel, message):
-    channel.send(cipher.encrypt(message)) # BAD: weak encryption
+    channel.send(cipher.encrypt(message))
 
 
 cipher = AES.new(SECRET_KEY)
 
 def send_encrypted(channel, message):
-    channel.send(cipher.encrypt(message)) # GOOD: strong encryption
+    channel.send(cipher.encrypt(message))
