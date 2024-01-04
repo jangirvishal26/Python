63c63def0e4b7d5c5c23e25875cb2e8d1ec9aadf
qafdevsec <111730727+qafdevsec@users.noreply.github.com>
2024-01-04 11:58:09 +0530

    Update crypto_issues_SF.py

--- CryptographicIssues/crypto_issues_SF.py
+++ CryptographicIssues/crypto_issues_SF.py
@@ -1,12 +1,23 @@
-from Crypto.Cipher import DES, AES
+import hashlib
 
-cipher = DES.new(SECRET_KEY)
+def insecure_hash_password(password):
+    # WARNING: Insecure use of the MD5 hash function
+    hashed_password = hashlib.md5(password.encode()).hexdigest()
+    return hashed_password
 
-def send_encrypted(channel, message):
-    channel.send(cipher.encrypt(message))
+def verify_password(incoming_password, stored_hashed_password):
+    # Verify the password by comparing the stored hash with the hash of the incoming password
+    return insecure_hash_password(incoming_password) == stored_hashed_password
 
+# Example usage (insecure)
+user_password = "my_secure_password"
+stored_hashed_password = insecure_hash_password(user_password)
 
-cipher = AES.new(SECRET_KEY)
+# Simulate a login attempt
+incoming_password_attempt = "attacker_attempting_to_guess_password"
+login_successful = verify_password(incoming_password_attempt, stored_hashed_password)
 
-def send_encrypted(channel, message):
-    channel.send(cipher.encrypt(message))
+if login_successful:
+    print("Login successful")
+else:
+    print("Login failed")
