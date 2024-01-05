8d8ac6550ecfdfd328bb31e1d8f4f500b62d6cc9
jangirvishal26 <52908574+jangirvishal26@users.noreply.github.com>
2024-01-05 12:35:02 +0530

    Update Arbitrary_file_write_tarfile_extraction.py

--- PathTravesal/Arbitrary_file_write_tarfile_extraction.py
+++ PathTravesal/Arbitrary_file_write_tarfile_extraction.py
@@ -1,7 +1,19 @@
 import sys
 import tarfile
 
-with tarfile.open(sys.argv[1]) as tar:
-    #BAD : This could write any file on the filesystem.
+def extract_tar_file(tar_filename, destination):
+    with tarfile.open(tar_filename) as tar:
+        extract_entries(tar, destination)
+
+def extract_entries(tar, destination):
+    # BAD: This could write any file on the filesystem.
     for entry in tar:
-        tar.extract(entry, "/tmp/unpack/")
+        tar.extract(entry, destination)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <tar_filename>")
+        sys.exit(1)
+
+    tar_filename = sys.argv[1]
+    extract_tar_file(tar_filename, "/tmp/unpack/")
