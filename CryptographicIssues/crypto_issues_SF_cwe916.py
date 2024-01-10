58943f087c36666f0eb389bd630875b7fb36bf1c
jangirvishal26 <52908574+jangirvishal26@users.noreply.github.com>
2024-01-10 11:24:06 +0530

    Update crypto_issues_SF_cwe916.py

--- CryptographicIssues/crypto_issues_SF_cwe916.py
+++ CryptographicIssues/crypto_issues_SF_cwe916.py
@@ -1,15 +1,13 @@
 import hashlib
 
 def insecure_hash_password(password):
-    # WARNING: Insecure use of the MD5 hash function
     hashed_password = hashlib.md5(password.encode()).hexdigest()
     return hashed_password
 
 def verify_password(incoming_password, stored_hashed_password):
-    # Verify the password by comparing the stored hash with the hash of the incoming password
     return insecure_hash_password(incoming_password) == stored_hashed_password
 
-# Example usage (insecure)
+
 user_password = "my_secure_password"
 stored_hashed_password = insecure_hash_password(user_password)
 
