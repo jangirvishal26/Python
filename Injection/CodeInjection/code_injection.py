0fc1d2f60095aa05f92e4ec923edb564f1f6167f
jangirvishal26 <52908574+jangirvishal26@users.noreply.github.com>
2024-01-10 19:10:56 +0530

    Update code_injection.py

--- Injection/CodeInjection/code_injection.py
+++ Injection/CodeInjection/code_injection.py
@@ -1,17 +1,34 @@
+from django.urls import path
+from django.http import HttpResponse
+import base64
+
 urlpatterns = [
     # Route to code_execution
-    url(r'^code-ex1$', code_execution_bad, name='code-execution-bad'),
-    url(r'^code-ex2$', code_execution_good, name='code-execution-good')
+    path('code-ex1', code_execution_bad, name='code-execution-bad'),
+    path('code-ex2', code_execution_good, name='code-execution-good')
 ]
 
-def code_execution(request):
+def get_user_input(request):
     if request.method == 'POST':
-        first_name = base64.decodestring(request.POST.get('first_name', ''))
-        #BAD -- Allow user to define code to be run.
-        exec("setname('%s')" % first_name)
+        return base64.decodestring(request.POST.get('first_name', ''))
+    return None
 
-def code_execution(request):
-    if request.method == 'POST':
-        first_name = base64.decodestring(request.POST.get('first_name', ''))
-        #GOOD --Call code directly
-        setname(first_name)
+def execute_code_bad(user_input):
+    # BAD -- Allow user to define code to be run.
+    exec("setname('%s')" % user_input)
+
+def execute_code_good(user_input):
+    # GOOD -- Call code directly
+    setname(user_input)
+
+def code_execution_bad(request):
+    user_input = get_user_input(request)
+    if user_input:
+        execute_code_bad(user_input)
+    return HttpResponse("Code executed successfully")
+
+def code_execution_good(request):
+    user_input = get_user_input(request)
+    if user_input:
+        execute_code_good(user_input)
+    return HttpResponse("Code executed successfully")
