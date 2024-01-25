74b347e82ac5a3b9c78273a6e28046935f76264f
jangirvishal26 <52908574+jangirvishal26@users.noreply.github.com>
2024-01-25 14:42:30 +0530

    Update ldap_query.py

--- Special Elements in Data Query Logic/ldap_query.py
+++ Special Elements in Data Query Logic/ldap_query.py
@@ -1,5 +1,6 @@
 from flask import request, Flask
 import ldap
+import ldap3
 
 
 @app.route("/normal")
@@ -13,3 +14,21 @@ def normal():
     ldap_connection = ldap.initialize("ldap://127.0.0.1")
     user = ldap_connection.search_s(
         dn, ldap.SCOPE_SUBTREE, search_filter)
+
+
+
+
+@app.route("/normal")
+def normal_test():
+    unsafe_dc = request.args['dc']
+    unsafe_filter = request.args['username']
+
+    dn = "dc={}".format(unsafe_dc)
+    search_filter = "(user={})".format(unsafe_filter)
+
+    srv = ldap3.Server('ldap://127.0.0.1')
+    conn = ldap3.Connection(srv, user=dn, auto_bind=True)
+    conn.search(dn, search_filter)
+
+normal()
+normal_test()
