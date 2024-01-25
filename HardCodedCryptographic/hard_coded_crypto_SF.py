9f1c17271c7ca6f6880555758779e92e8391bbff
jangirvishal26 <52908574+jangirvishal26@users.noreply.github.com>
2024-01-25 11:55:35 +0530

    Update hard_coded_crypto_SF.py

--- HardCodedCryptographic/hard_coded_crypto_SF.py
+++ HardCodedCryptographic/hard_coded_crypto_SF.py
@@ -1,16 +1,3 @@
-# def verify_admin(password):
-#     if password != "68af404b513073584c4b6f22b6c63e6b":
-#         print("Incorrect Password!")
-#         return 0
-
-#     print("Entering Diagnostic Mode...")
-#     return 1
-
-# # Example usage:
-# password_input = input("Enter the password: ")
-# result = verify_admin(password_input)
-# print("Verification result:", result)
-
 def verify_admin(password):
     # In a real scenario, the hashed password would be stored securely,
     # and we would use a secure password hashing library for comparison.
