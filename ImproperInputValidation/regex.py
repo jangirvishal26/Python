b31fdac7d4501d4e6a2fb0072a382b476b8b6063
jangirvishal26 <52908574+jangirvishal26@users.noreply.github.com>
2024-01-25 11:22:35 +0530

    Update regex.py

--- ImproperInputValidation/regex.py
+++ ImproperInputValidation/regex.py
@@ -1,12 +1,14 @@
 import re
+import webbrowser
 
 def unsafe_redirect(input_url):
     # Unsafe regular expression
     unsafe_regex = re.compile("(www|beta).example.com/")
 
-    if unsafe_regex.match(input_url):
+    if unsafe_regex.search(input_url):
         print(f"Redirecting to: {input_url}")
-        # Perform the redirect (simulate redirection)
+        # Perform the redirect using webbrowser.open_new_tab (simulate redirection)
+        webbrowser.open_new_tab(input_url)
     else:
         print("Invalid redirect target")
 
