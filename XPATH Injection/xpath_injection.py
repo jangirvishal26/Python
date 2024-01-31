faebb84dbf3637d066a21caab119785c1bf82eff
jangirvishal26 <52908574+jangirvishal26@users.noreply.github.com>
2024-01-31 14:49:55 +0530

    Update xpath_injection.py
    cwe 643

--- XPATH Injection/xpath_injection.py
+++ XPATH Injection/xpath_injection.py
@@ -1,18 +1,31 @@
 from lxml import etree
 from io import StringIO
 
-from django.urls import path
-from django.http import HttpResponse
-from django.template import Template, Context, Engine, engines
-
-
-def a(request):
-    value = request.GET['xpath']
-    f = StringIO('<foo><bar></bar></foo>')
+def perform_xpath_query(xml_data, user_input):
+    # Source: Sensitive XML data obtained from a source
+    f = StringIO(xml_data)
     tree = etree.parse(f)
-    r = tree.xpath("/tag[@id='%s']" % value)
 
+    # Sink: User input concatenated into the XPath query (vulnerable)
+    result = tree.xpath("/data[@id='%s']" % user_input)
+    return result
 
-urlpatterns = [
-    path('a', a)
-]
+def process_result(result):
+    # Process the result obtained from the XPath query
+    print("Processed result:", result)
+
+def main():
+    # Simulating sensitive XML data
+    xml_data = '<data id="123">Sensitive Data</data>'
+
+    # Simulating user input (potentially from an untrusted source)
+    user_input = "123' or '1'='1'"
+
+    # Performing an XPath query with user input
+    query_result = perform_xpath_query(xml_data, user_input)
+
+    # Processing the result (may contain unintended data due to injection)
+    process_result(query_result)
+
+if __name__ == "__main__":
+    main()
