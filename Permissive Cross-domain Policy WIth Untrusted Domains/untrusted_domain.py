e2dacc89609faa3d9b43968e8aab9dec775f1af3
jangirvishal26 <52908574+jangirvishal26@users.noreply.github.com>
2024-01-31 12:47:05 +0530

    Update untrusted_domain.py

--- Permissive Cross-domain Policy WIth Untrusted Domains/untrusted_domain.py
+++ Permissive Cross-domain Policy WIth Untrusted Domains/untrusted_domain.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-# This route renders a page that loads resources from any domain (unsafe)
+# Set the template folder to the current directory
+app.template_folder = os.path.dirname(os.path.realpath(__file__))
+
 @app.route('/')
 def index():
     return render_template('index.html')
