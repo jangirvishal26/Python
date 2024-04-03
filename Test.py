21d88d1ae6597003df9938683d37281228e40c06
Vishal Jangir <vjangir@fortinet-us.com>
2024-04-03 15:13:07 +0530

    added missing cwe

--- Test.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-import os
-
-with zipfile.ZipFile('archive.zip', 'r') as zip_ref:
-    for file_info in zip_ref.infolist():
-        file_name = file_info.filename
-        # WARNING: This could write any file on the filesystem.
-        with zip_ref.open(file_name) as source, open(file_name, 'wb') as sink:
-            sink.write(source.read())
